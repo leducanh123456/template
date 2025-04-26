a246e35ca66be7d712e5e2c250b875076ea06c35
leducanh.anhld <leducanh.anhld@gmail.com>
2025-04-26 14:10:51 +0700

    first commit

---
+++ src/test/java/io/github/leducanh123456/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leducanh123456.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
